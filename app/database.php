686ceedb1e7da6a72ce623c456205de5f0beb49f
哞菇 <i@flag.moe>
2015-10-06 20:32:18 +0800

    database.php can worked

--- app/database.php
+++ app/database.php
@@ -3,9 +3,11 @@
  * Database
  *
  * @author NekoTora <i@flag.moe>
- * 数据库调教
+ * 
+ * 哞菇的测试型数据库调教姬。
  * 
  */
+
 require_once 'config.php';
 class DB {
 
@@ -14,20 +16,20 @@ class DB {
 
 
 	function __construct() {
-		echo '嘿！Database蹦迪中！';
 		//建立连接
 		$this->_db = new mysqli(DB_SERVER, DB_USER, DB_PWD, DB_NAME);
+		//噫~竟然连不上
 		if ($this->_db->connect_error) {
 			die(json_encode(array(
 				'status' => 'faild',
-				'message' => 'Database Connect Error'
+				'message' => 'Database Connect Error (' . DB::$_db->connect_errno . ') ' . DB::$_db->connect_error
 			)));
 		}
 	}
 
-	//查找数据
+	//查找
 	function find($table, $conditions = [], $page = NULL, $size = NULL) {
-		//准备一个sql
+		//准备一个sql模版
 		$template = ' SELECT * FROM `' . $table . '` ';
 		//看看有几个条件扔进语句，用?代替一下值
 		$args = [];
@@ -44,17 +46,17 @@ class DB {
 				}
 			}
 		}
-		//嘿！想分页吗？
+		//想分页吗？
 		if ($page != NULL && $size != NULL) {
 			$template .= ' LIMIT ' . ($page - 1) * $size . ',' . $size;
 		}
-		//丢过去
+		//丢过去执行
 		return $this->query($template, $args);
 	}
 
-	//插入数据
+	//添加
 	function insert($table, $items = []) {
-		//准备一个sql
+		//准备一个sql模版
 		$template = ' INSERT INTO `' . $table . '` ';
 		//看看都有啥数据要存，同理
 		$args = [];
@@ -82,35 +84,37 @@ class DB {
 	//检查数据，搞一个大新闻
 	function query($query, $args = []) {
 		// SELECT * FROM `xxx` WHERE `xx` = '12' AND `xx` = ?
-		
 		$flag = 0;
 		while($pos = strpos($query, '?')) {
+			//没吃药的话会参数不够
 			if (!isset($args[$flag])) die(json_encode(array(
 				'status' => 'faild',
-				'message' => 'Oppos!'
+				'message' => 'Agrument Error ( No more argument can fill into query )'
 			)));
+
 			//安全第一
 			$query = substr($query, 0, $pos) . '\'' .
-			$_db->real_escape_string($args[$count]) . 
-			'\'' . substr($query, $pos + 1);
-
-			$count++;
+			$this->_db->real_escape_string($args[$flag]) . 
+				'\'' . substr($query, $pos + 1);
+			$flag++;
 		}
-		return $this->pack($query);
+		//走你
+		return $this->pack($this->_db->query($query));
 	}
 
-	//快递到家，送到派物流
+	//三期食堂派物流代收
 	function pack($query_result){
-		//卧槽没结果？！西奈！
+		var_dump($query_result);
+		//卧槽没结果？死ね！
 		if (!$query_result) die(json_encode(array(
 			'status' => 'faild',
-			'message' => 'Query Error'
+			'message' => 'Query Error (' . DB::$_db->error . ')'
 		)));
-			var_dump($query_result);
 		//快递打包
 		$result = new StdClass;
-		if (is_object($query_result)) {//object
-			$result->count = $query_result->num_rows;
+		//object的话就翻译成json
+		if (is_object($query_result)) {
+			$result->flag = $query_result->num_rows;
 			$result->objs = [];
 			while($t = $query_result->fetch_object()) {
 				$result->objs[] = $t;
@@ -118,12 +122,13 @@ class DB {
 			$result->json = json_encode($result->objs);
 			$result->raw = $query_result;
 		} else {
-			$result->count = 1;
+			$result->flag = 1;
 			$result->objs = ['status' => 'success'];
 			$result->json = json_encode($result->objs);
 			$result->raw = $query_result;
-			return $result;
 		}
+		//出炉啦~
+		return $result;
 	}
 
 	//事后……断开连接
@@ -133,4 +138,9 @@ class DB {
 }
 
 $test = new DB();
-var_dump($test->find('nekonote', [], 1, 20));
+
+var_dump($test->insert('note', [
+		'time' => time(),
+		'content' => htmlspecialchars('try'),
+		'user' => 'adminre'
+	]));
